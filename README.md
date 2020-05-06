387b4bb6aa6b45a57c190df415f4186a4e499a92
UnknownException <UnknownException@users.noreply.github.com>
2020-05-06 14:41:26 +0200

    Scale gamefield and reposition bottom menu

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Unofficial Submarine Titans patch
 
 Unfinished
+
+* High DPI causes issues
+* Repositioning (value) of bottom menu is currently hardcoded
--- subtitans/patches.cpp
+++ subtitans/patches.cpp
@@ -18,10 +18,63 @@ bool WindowedMode()
 	return true;
 }
 
+// Detour variables
+unsigned long constexpr NativeResolution_RepositionBottomMenu_DetourSize = 26;
+unsigned long constexpr NativeResolution_RepositionBottomMenu_JmpFrom = 0x004F8084;
+unsigned long constexpr NativeResolution_RepositionBottomMenu_JmpBack = NativeResolution_RepositionBottomMenu_JmpFrom + NativeResolution_RepositionBottomMenu_DetourSize;
+__declspec(naked) void NativeResolution_RepositionBottomMenu()
+{
+	__asm mov ecx, 0x0A 
+	__asm mov dword ptr ds:[esi + 0x60], edx
+
+	__asm mov edx, dword ptr ds:[esi + 0x8C]; 
+	__asm cmp edx, 0xF0; // Only do this for 1280x1024 resolution modification
+	__asm jne ASM_NR_LOOP;
+	__asm add ecx, 0x01; // + 1 for Left TV panel repositioning (orig 0a)
+
+ASM_NR_LOOP:
+	__asm mov edx, dword ptr ds:[esi + 0x8C];
+	__asm cmp edx, 0xF0; // Only do this for 1280x1024 resolution modification
+	__asm jne ASM_NR_NOTNATIVERES;
+	__asm cmp ecx, 1 // Check if last (0x0B: Left TV panel) <-- Don't add default value if match
+	__asm jne ASM_NR_DONTOVERRIDEDEFAULT
+	__asm mov edx, 0x00
+
+ASM_NR_DONTOVERRIDEDEFAULT:
+	__asm add edx, 0x140; // Widescreen reposition value
+
+ASM_NR_NOTNATIVERES:
+	// Restore
+	__asm mov ebx, dword ptr ds:[eax];
+	__asm add ebx, edx;
+	__asm mov dword ptr ds:[eax], ebx;
+	__asm add eax, 4;
+	__asm dec ecx;
+	__asm jnz ASM_NR_LOOP;
+
+	__asm jmp NativeResolution_RepositionBottomMenu_JmpBack;
+}
+
+// Overwriting 1280x1024
 bool NativeResolution()
 {
-	int screenWidth = 1920;
-	int screenHeight = 1200;
+	DEVMODE deviceMode;
+	ZeroMemory(&deviceMode, sizeof(deviceMode));
+	deviceMode.dmSize = sizeof(DEVMODE);
+	if (!EnumDisplaySettings(NULL, ENUM_CURRENT_SETTINGS, &deviceMode))
+		return false;
+
+	int screenWidth = deviceMode.dmPelsWidth;
+	int screenHeight = deviceMode.dmPelsHeight;
+
+	if (screenWidth < 1280 || (screenWidth == 1280 && screenHeight == 1024)) // Don't patch
+		return true;
+
+	if (screenWidth == 1366 && screenHeight == 768)
+	{
+		MessageBox(NULL, L"1366x768 causes rendering issues; trying 1280x768", L"Warning", MB_ICONWARNING);
+		screenWidth = 1280;
+	}
 
 	unsigned char buffer[4];
 	
@@ -35,17 +88,17 @@ bool NativeResolution()
 	if (!MemoryWriter::Write(0x0056C6CF, buffer, sizeof(int)))
 		return false;
 
-	// Unknown width 1
+	// GUI Rescaler?
 	memcpy(buffer, &screenWidth, sizeof(int));
 	if (!MemoryWriter::Write(0x004F3254, buffer, sizeof(int)))
 		return false;
 
-	// Unknown width 2
+	// Queuescreen bottom @ ingame patch (0: 800, 2:1024, 6: 1280)
 	memcpy(buffer, &screenWidth, sizeof(int));
 	if (!MemoryWriter::Write(0x004F9CCB, buffer, sizeof(int)))
 		return false;
 
-	// Unknown width + height 3
+	// Initial screen width/height
 	memcpy(buffer, &screenWidth, sizeof(int));
 	if (!MemoryWriter::Write(0x0053202B, buffer, sizeof(int)))
 		return false;
@@ -54,18 +107,45 @@ bool NativeResolution()
 	if (!MemoryWriter::Write(0x00532032, buffer, sizeof(int)))
 		return false;
 
-	// Unknown width 4
+	// Screen resize compare value
 	memcpy(buffer, &screenWidth, sizeof(int));
 	if (!MemoryWriter::Write(0x0056EFCA, buffer, sizeof(int)))
 		return false;
 
-	// Unknown width + height 5
+	// Screen resize values (width, height)
 	memcpy(buffer, &screenWidth, sizeof(int));
 	if (!MemoryWriter::Write(0x0056F035, buffer, sizeof(int)))
 		return false;
 
 	memcpy(buffer, &screenHeight, sizeof(int));
 	if (!MemoryWriter::Write(0x0056F03F, buffer, sizeof(int)))
+		return false;
+
+	// Gamefield preset values (width, height)
+	int fieldWidth = screenWidth - 40;
+	memcpy(buffer, &fieldWidth, sizeof(int));
+	if (!MemoryWriter::Write(0x0056CA0C, buffer, sizeof(int)))
+		return false;
+
+	int fieldHeight = screenHeight - 62;
+	memcpy(buffer, &fieldHeight, sizeof(int));
+	if (!MemoryWriter::Write(0x0056CA16, buffer, sizeof(int)))
+		return false;
+
+	// The movie resolution patch does NOT scale the movie
+	// Perhaps try to fix this someday?
+
+	// Movie resolution patch (Width)
+	memcpy(buffer, &screenWidth, sizeof(int));
+	if (!MemoryWriter::Write(0x00571D55, buffer, sizeof(int)))
+		return false;
+
+	// Movie resolution patch (Height)
+	memcpy(buffer, &screenHeight, sizeof(int));
+	if (!MemoryWriter::Write(0x00571D5C, buffer, sizeof(int)))
+		return false;
+
+	if (!Detour::Create(NativeResolution_RepositionBottomMenu_JmpFrom, NativeResolution_RepositionBottomMenu_DetourSize, (unsigned long)NativeResolution_RepositionBottomMenu))
 		return false;
 
 	return true;
@@ -77,7 +157,7 @@ bool NativeResolution()
 // Detour variables
 unsigned long constexpr SleepWell_Implementation_DetourSize = 10;
 unsigned long constexpr SleepWell_Implementation_JmpFrom = 0x006E6253;
-unsigned long constexpr SleepWell_Implementation_JmpBack = 0x006E6253 + SleepWell_Implementation_DetourSize;
+unsigned long constexpr SleepWell_Implementation_JmpBack = SleepWell_Implementation_JmpFrom + SleepWell_Implementation_DetourSize;
 
 // Function specific variables
 static unsigned long SleepWell_Implementation_CurrentTime = timeGetTime();
@@ -95,7 +175,7 @@ __declspec(naked) void SleepWell_Implementation()
 	if (std::abs((long)SleepWell_Implementation_CurrentTime - (long)SleepWell_Implementation_PreviousTime) > 1000)
 	{
 #ifdef _DEBUG
-		MessageBox(NULL, L"Rendering is behind schedule", L"FPS Limiter", MB_ICONINFORMATION);
+//		MessageBox(NULL, L"Rendering is behind schedule", L"FPS Limiter", MB_ICONINFORMATION);
 #endif
 		SleepWell_Implementation_PreviousTime = SleepWell_Implementation_CurrentTime - SleepWell_Implementation_FrameLimit;
 	}
@@ -117,7 +197,7 @@ bool SleepWell()
 {
 	if (!Detour::Create(SleepWell_Implementation_JmpFrom, SleepWell_Implementation_DetourSize, (unsigned long)SleepWell_Implementation))
 		return false;
-
+	
 	return true;
 }
 
@@ -129,23 +209,23 @@ bool Patches::Apply()
 		return false;
 	}
 
-#ifdef _DEBUG
+//#ifdef _DEBUG
 	// TODO Lots
 	if (!NativeResolution())
 	{
 		MessageBox(NULL, L"Failed to apply Native Resolution patch", L"Patching error", MB_ICONERROR);
 		return false;
 	}
-#endif
+//#endif
 
-#ifdef _DEBUG
+//#ifdef _DEBUG
 	// TODO Menu effects (Sleep based timing?) & Decoupling logics/rendering
 	if (!SleepWell())
 	{
 		MessageBox(NULL, L"Failed to apply Sleep Well patch", L"Patching error", MB_ICONERROR);
 		return false;
 	}
-#endif
+//#endif
 
 	return true;
 }
